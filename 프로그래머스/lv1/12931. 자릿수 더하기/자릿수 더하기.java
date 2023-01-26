39399c2fb8ed2a81c90f007d646051990f854022
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-01-26 22:18:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0){
+            answer += n%10;
+            n /= 10;
+        }
+        return answer;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        System.out.println(sol.solution(123));
+    }
+}
