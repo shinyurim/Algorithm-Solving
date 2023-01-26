3f1cb67f7de3e1159f1b9eda279a2235ee8c5293
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-01-26 22:11:43 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+	public int solution(int num1, int num2) {
+		int answer = num1 + num2;
+
+		return answer;
+	}
+	public static void main(String args[]){
+
+		Solution sol = new Solution();
+		int num1 = 50000;
+		int num2 = 50000;
+
+		System.out.println(sol.solution(num1,num2));
+	}
+}
