a3f4a0c0d7ef0b93d977048462fbabb151bf0dd1
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-03-05 22:59:06 +0900

    [Bronze II] Title: A+B - 6, Time: 232 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            String[] str = sc.next().split(",");
+            System.out.println(Integer.parseInt(str[0]) + Integer.parseInt(str[1]));
+        }
+    }
+}
