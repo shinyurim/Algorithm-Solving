91fb12512b509c7087f51ddaa2234fc45c3688c1
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-03-18 00:37:43 +0900

    [Silver IV] Title: 괄호, Time: 244 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -5,31 +5,31 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
-        Stack<Character> stack = new Stack();
+        Stack<Character> stack = new Stack<>();
         String s = sc.nextLine();
 
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < T; i++) { //테스트 데이터의 갯수만큼 for문을 돌리면서
             s = sc.nextLine();
             for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-                if(stack.isEmpty()) stack.push(c);
-                else {
-                    if(c == ')') {
-                        if(stack.peek() == '(') {
-                            stack.pop();
+                char c = s.charAt(j); // j번째 인덱스를 하나씩 받아
+                if(stack.isEmpty()) stack.push(c); // 만약 스택이 비어있으면 해당 문자열을 stack에 추가
+                else { // 스택이 비어있지 않은 경우
+                    if(c == ')') { // 만약 c가 ')'일때
+                        if(stack.peek() == '(') { // 스택에 들어갔던 마지막 문자가 '('이면
+                            stack.pop(); // pop을 이용해서 마지막에 들어갔던 문자 제거
                         }
-                        else {
-                            stack.push(c);
+                        else { // 스택에 들어갔던 마지막 문자가 '('가 아니면
+                            stack.push(c); // 스택에 c 문자열을 추가
                         }
-                    }else { // (s.charAt(j) == '(')
-                        stack.push(c);
+                    }else { // 그 외의 경우
+                        stack.push(c); // 스택에 c 문자열을 추가
                     }
                 }
             }
-            if(stack.isEmpty()) {
-                System.out.println("YES");
-            }else {
-                System.out.println("NO");
+            if(stack.isEmpty()) { // 만약 스택이 비어있다면 올바른 문자열이라고 판단
+                System.out.println("YES"); // yes출력
+            }else { // 비어있지 않다면 올바르지 않은 문자열이라고 판단
+                System.out.println("NO"); // no출력
             }
             stack.clear();
         }
