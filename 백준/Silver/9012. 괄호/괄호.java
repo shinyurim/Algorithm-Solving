765404bc2a0f0a61e045e0b867903ba495243776
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-03-18 00:19:32 +0900

    [Silver IV] Title: 괄호, Time: 252 ms, Memory: 18108 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        Stack<Character> stack = new Stack();
+        String s = sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            s = sc.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if(stack.isEmpty()) stack.push(c);
+                else {
+                    if(c == ')') {
+                        if(stack.peek() == '(') {
+                            stack.pop();
+                        }
+                        else {
+                            stack.push(c);
+                        }
+                    }else { // (s.charAt(j) == '(')
+                        stack.push(c);
+                    }
+                }
+            }
+            if(stack.isEmpty()) {
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+            stack.clear();
+        }
+    }
+}
