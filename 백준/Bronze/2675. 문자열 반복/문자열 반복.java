b1c042061230450acfdc76b3b5223aac52364a93
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-03-19 23:43:57 +0900

    [Bronze II] Title: 문자열 반복, Time: 224 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -7,14 +7,14 @@ public class Main {
         int T = sc.nextInt(); // 테스트 케이스의 갯수
 
         for (int i = 0; i < T; i++) {
-            int R = sc.nextInt(); // 몇 번 반복할건지?
-            String S = sc.next(); // 문자 뭐 넣을건지?
+            int R = sc.nextInt(); // 반복횟수
+            String s = sc.next(); // 문자열 받아주기
 
-            for (int j = 0; j < S.length(); j++) { 
-                for (int k = 0; k < R; k++) { 
-                    System.out.print(S.charAt(j)); 
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    System.out.print(s.charAt(j));
                 }
-                if (j == S.length()-1){ // 만약 문자열의 마지막 인덱스까지 확인하면
+                if (j == s.length()-1){ // 만약 문자열의 마지막 인덱스까지 확인하면
                     System.out.println(); // 줄바꿈
                 }
             }
