3952ddd3a1c131e295f4778eb8a252823703c70a
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-05-03 22:48:38 +0900

    [level 3] Title: 야근 지수, Time: 107.11 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,31 @@
+
+// 야근 피로도 = 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한값
+// 최소의 야근 피로도를 구하는 문제.
+// 우선순위 큐
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 우선순위 큐 선언 + 내림차순 정렬
+
+        for (int j : works) {
+            pq.offer(j);
+        }
+
+        while (n > 0) {
+            int work = pq.poll();
+            if (work == 0) break;
+            work -= 1;
+            pq.offer(work);
+            n -= 1;
+        }
+
+        for (int work : pq) {
+            answer += (long) work * work;
+        }
+        return answer;
+    }
+}
