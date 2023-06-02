4a23329a77831fa4a66cd21f3ed443d78891ccfb
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-06-02 23:37:28 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int cnt = 0;
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        for (int i = 0;i < names.length;i+=5)
+            answer[cnt++] = names[i];
+        return answer;
+    }
+}
