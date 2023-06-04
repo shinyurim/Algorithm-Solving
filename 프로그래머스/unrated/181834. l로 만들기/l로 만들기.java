0f81096ce5c4edc7da646047b13347328f19fa36
shinyurim <108784880+shinyurim@users.noreply.github.com>
2023-06-04 23:18:08 +0900

    [unrated] Title: l로 만들기, Time: 5.71 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuffer sb = new StringBuffer();
+        
+        for (char ch : myString.toCharArray()) {
+            if (ch < 'l') sb.append('l');
+            else sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
